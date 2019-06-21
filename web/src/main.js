2402c7bed2ef1bac55ca80a520c942ca5b3feff6
yaowuya <zrfyaowuya@126.com>
2019-06-21 18:32:26 +0800

    web 配置

--- web/src/main.js
+++ web/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'amfe-flexible/index.js'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
